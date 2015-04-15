167590fd1abbd96a70901b70dafb85eb00ad0be6
Diogo Alves <diogoalvessilva86@gmail.com>
2015-04-15 17:14:51 -0300

    Teste para fazer o login do usuário, ainda falta verificar se o usuário está logado.

--- behat3/features/bootstrap/UsuarioContext.php
+++ behat3/features/bootstrap/UsuarioContext.php
@@ -1,5 +1,6 @@
 <?php
-
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Behat\Context\ClosuredContextInterface,
     Behat\Behat\Context\TranslatedContextInterface,
     Behat\Behat\Context\BehatContext,
@@ -13,14 +14,51 @@ use Behat\Behat\Context\Step;
 /**
  * Usuario context.
  */
-class UsuarioContext extends MinkContext
+class UsuarioContext extends MinkContext implements Context
 {
 
     /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
+     * Efetua login do usuário.
+     * @Then faço login 
+    */
+    public function doLogin()
+    {
+        try {
+            $this->getSession()->getDriver()->executeScript("
+                    Login.efetuaLogin('FormPOPLogin');
+            ");
+         
+        } catch (Exception $e) {
+            throw new Exception("Ocorreu um erro fatal ao efetuar o login.\nInformações detalhadas do erro: ".$e->getMessage());
+        }
+    }
+
+    /**
+    * Verifica se o usuário atual está logado.
+    * @Then verifico se estou logado
+    */
+    public function verifyLogin()
+    {
+        try {
+            $this->visit("/astrologia/horoscopo");    
+            $estaLogado = $this->assertPageContainsText("Não é Cadastro? Desconectar");
+            if (!$estaLogado)
+                throw new Exception("O usuário não está logado. \n O teste será interrompido.");
+                
+        } catch (Exception $e) {
+            throw new Exception("Ocorreu um erro fatal ao verificar o login.\nInformações detalhadas do erro: ".$e->getMessage());   
+        }
+    }
+
+    /**
+     * @Given preencho com :arg1 o campo :arg2
      */
+    public function fillFieldWithSelector($valor, $campo)
+    {
+        try {
+            $this->fillField($campo, $valor);
+        } catch (Exception $e) {
+            throw new Exception("Não foi possível preencher o campo ".$campo.".\nInformações detalhadas do erro: ".$e->getMessage());
+        }
+    }
 }
--- behat3/features/usuario/LoginUsuario.feature
+++ behat3/features/usuario/LoginUsuario.feature
@@ -2,7 +2,9 @@
 Funcionalidade: Login
 @javascript
 Cenário: Usuário possui cadastro e está na página inicial
-	Dado Eu Estou em "/login?ReturnToURL=L2FzdHJvbG9naWEvaG9yb3Njb3Bv"
-	E Eu preencho "txEmail" com "behattestedecadastro@hotmail.com"
-	E Eu preencho “txSenha” com "1234"
-	E Faco Login
+	Dado Eu estou na página de entrada
+	Quando vou para "/login?ReturnToURL=L2FzdHJvbG9naWEvaG9yb3Njb3Bv"
+	E preencho com "behattestedecadastro@hotmail.com" o campo "txEmail" 
+	E preencho com "1234" o campo "pwPassword" 
+	Quando faço login
+	Então verifico se estou logado
