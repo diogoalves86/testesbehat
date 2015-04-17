87b93db8eb9e848b1fd7914b4e323bcd74a2d4f9
Diogo Alves <diogoalvessilva86@gmail.com>
2015-04-17 17:49:30 -0300

    Adicionando o contexto geral do Personare.

---
+++ behat3/features/bootstrap/PersonareContext.php
@@ -0,0 +1,37 @@
+<?php 
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+ 
+use Behat\MinkExtension\Context\MinkContext;
+use Behat\Behat\Context\Step;
+use Behat\Behat\Context\Context;
+
+/**
+* Classe responsável pelo contexto geral do Personare
+*/
+class PersonareContext extends MinkContext
+{
+	
+    /*
+    * Este método tem o papel de logar o usuário no caso de testes que precisem do usuário logado.
+    * Por exemplo, os testes de carrinho. 
+    */
+    public function doLoginByUserAndPassword($username, $password)
+    {
+        try {
+            $this->visit("/login");
+            $this->fillField("txEmail");
+            $this->fillField("pwPassword");
+            $this->getSession()->getDriver()->executeScript("
+                    Login.efetuaLogin('FormPOPLogin');
+            ");
+            
+        } catch (Exception $e) {
+            throw new Exception("Não foi realizar o login do usuário ".$username.".\nInformações detalhadas do erro: ".$e->getMessage());   
+        }
+    }
+}
