345f6ce24e3581582a286511b20bd2d5b747b121
Diogo Alves <diogoalvessilva86@gmail.com>
2015-04-17 17:12:09 -0300

    Adicionando o contexto do carrinho.

---
+++ behat3/features/bootstrap/CarrinhoContext.php
@@ -0,0 +1,19 @@
+<?php
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+ 
+use Behat\MinkExtension\Context\MinkContext;
+use Behat\Behat\Context\Step;
+ 
+/**
+ * Carrinho context.
+ */
+class CarrinhoContext extends MinkContext
+{
+
+}
+
